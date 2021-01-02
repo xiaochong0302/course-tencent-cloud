26deccc36b4d6b48783a31540246be2c49ca991c
xiaochong0302 <xiaochong0302@gmail.com>
2021-01-02 13:31:13 +0800

    增加限制共享帐号功能

---
+++ app/Repos/UserSession.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repos;
+
+use App\Models\UserSession as UserSessionModel;
+use Phalcon\Mvc\Model\Resultset;
+use Phalcon\Mvc\Model\ResultsetInterface;
+
+class UserSession extends Repository
+{
+
+    /**
+     * @param int $userId
+     * @return ResultsetInterface|Resultset|UserSessionModel[]
+     */
+    public function findByUserId($userId)
+    {
+        return UserSessionModel::query()
+            ->where('user_id = :user_id:', ['user_id' => $userId])
+            ->andWhere('deleted = 0')
+            ->execute();
+    }
+
+}
---
+++ db/migrations/20210102041941_schema_202101021220.php
@@ -0,0 +1,158 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class Schema202101021220 extends Phinx\Migration\AbstractMigration
+{
+
+    public function change()
+    {
+        $this->table('kg_user_session', [
+            'id' => false,
+            'primary_key' => ['id'],
+            'engine' => 'InnoDB',
+            'encoding' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci',
+            'comment' => '',
+            'row_format' => 'DYNAMIC',
+        ])
+            ->addColumn('id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'identity' => 'enable',
+                'comment' => '主键编号',
+            ])
+            ->addColumn('user_id', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '用户编号',
+                'after' => 'id',
+            ])
+            ->addColumn('session_id', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 64,
+                'collation' => 'utf8mb4_general_ci',
+                'encoding' => 'utf8mb4',
+                'comment' => '会话编号',
+                'after' => 'user_id',
+            ])
+            ->addColumn('client_type', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '终端类型',
+                'after' => 'session_id',
+            ])
+            ->addColumn('client_ip', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 64,
+                'collation' => 'utf8mb4_general_ci',
+                'encoding' => 'utf8mb4',
+                'comment' => '终端IP',
+                'after' => 'client_type',
+            ])
+            ->addColumn('deleted', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '删除标识',
+                'after' => 'client_ip',
+            ])
+            ->addColumn('create_time', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '创建时间',
+                'after' => 'deleted',
+            ])
+            ->addColumn('update_time', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '更新时间',
+                'after' => 'create_time',
+            ])
+            ->addIndex(['user_id'], [
+                'name' => 'user_id',
+                'unique' => false,
+            ])
+            ->create();
+        $this->table('kg_user_token', [
+            'id' => false,
+            'primary_key' => ['id'],
+            'engine' => 'InnoDB',
+            'encoding' => 'utf8mb4',
+            'collation' => 'utf8mb4_general_ci',
+            'comment' => '',
+            'row_format' => 'DYNAMIC',
+        ])
+            ->addColumn('id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'identity' => 'enable',
+                'comment' => '主键编号',
+            ])
+            ->addColumn('user_id', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '用户编号',
+                'after' => 'id',
+            ])
+            ->addColumn('token', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 64,
+                'collation' => 'utf8mb4_general_ci',
+                'encoding' => 'utf8mb4',
+                'comment' => '身份令牌',
+                'after' => 'user_id',
+            ])
+            ->addColumn('client_type', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '终端类型',
+                'after' => 'token',
+            ])
+            ->addColumn('client_ip', 'string', [
+                'null' => false,
+                'default' => '',
+                'limit' => 64,
+                'collation' => 'utf8mb4_general_ci',
+                'encoding' => 'utf8mb4',
+                'comment' => '终端IP',
+                'after' => 'client_type',
+            ])
+            ->addColumn('deleted', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '删除标识',
+                'after' => 'client_ip',
+            ])
+            ->addColumn('create_time', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '创建时间',
+                'after' => 'deleted',
+            ])
+            ->addColumn('update_time', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'comment' => '更新时间',
+                'after' => 'create_time',
+            ])
+            ->addIndex(['user_id'], [
+                'name' => 'user_id',
+                'unique' => false,
+            ])
+            ->create();
+    }
+
+}
