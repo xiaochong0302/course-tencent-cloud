2451aa3c005ef53d28a26b549ab778d8c223a4a8
koogua <xiaochong0302@gmail.com>
2021-03-05 18:59:35 +0800

    更新演示帐号

--- app/Console/Tasks/ResetDemoAccountTask.php
+++ app/Console/Tasks/ResetDemoAccountTask.php
@@ -36,7 +36,7 @@ class ResetDemoAccountTask extends Task
         $user = $userRepo->findById(100015);
 
         $user->name = '酷瓜云课堂';
-        $user->title = '页面重构设计';
+        $user->title = '首席音效师';
         $user->about = '酷瓜云课堂（腾讯云版），依托腾讯云基础服务架构，采用C扩展PHP框架Phalcon开发，致力开源网课系统，开源网校系统，开源在线教育系统。';
         $user->avatar = '/img/avatar/20210214084718217596.jpeg';
 
@@ -63,10 +63,10 @@ class ResetDemoAccountTask extends Task
 
         $user = $userRepo->findById(100065);
 
-        $user->name = 'Murphy';
-        $user->title = '移动开发工程师';
-        $user->about = '腾讯开放平台高级工程师 Web技术大牛 10年风雨开发路 腾讯学院讲师 沙龙论坛演讲大V T恤男 儒雅先生 精通C#、ASP．NET和SQL Server等 负责开发“QQ概念版”、“Q+”、“影视中心”、“QQ互联”等产品';
-        $user->avatar = '/img/avatar/202001251155458851.jpg';
+        $user->name = '酷瓜云小二';
+        $user->title = '首席茶艺师';
+        $user->about = '酷瓜云课堂，100%开源在线教育解决方案';
+        $user->avatar = '/img/avatar/default.png';
 
         $user->update();
     }
