2135f375e8dda1d2405ee58d8d05ddb3b5a6d801
koogua <xiaochong0302@gmail.com>
2021-09-30 21:00:03 +0800

    修正文章，提问，回答评论查询参数

--- app/Services/Logic/Question/CommentList.php
+++ app/Services/Logic/Question/CommentList.php
@@ -31,6 +31,8 @@ class CommentList extends LogicService
         $params['item_id'] = $question->id;
         $params['item_type'] = CommentModel::ITEM_QUESTION;
         $params['published'] = CommentModel::PUBLISH_APPROVED;
+        $params['parent_id'] = 0;
+        $params['deleted'] = 0;
 
         $sort = $pagerQuery->getSort();
         $page = $pagerQuery->getPage();
