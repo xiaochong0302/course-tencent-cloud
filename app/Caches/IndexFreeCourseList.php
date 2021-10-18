a85de0d8a4619c5386c1d59b14414e2f12637361
koogua <xiaochong0302@gmail.com>
2021-10-18 18:56:53 +0800

    修正首页缓存虚拟用户数问题

--- app/Caches/IndexFreeCourseList.php
+++ app/Caches/IndexFreeCourseList.php
@@ -65,6 +65,13 @@ class IndexFreeCourseList extends Cache
             $categoryCourses = [];
 
             foreach ($courses as $course) {
+
+                $userCount = $course->user_count;
+
+                if ($course->fake_user_count > $course->user_count) {
+                    $userCount = $course->fake_user_count;
+                }
+
                 $categoryCourses[] = [
                     'id' => $course->id,
                     'title' => $course->title,
@@ -73,7 +80,7 @@ class IndexFreeCourseList extends Cache
                     'vip_price' => $course->vip_price,
                     'model' => $course->model,
                     'level' => $course->level,
-                    'user_count' => $course->user_count,
+                    'user_count' => $userCount,
                     'lesson_count' => $course->lesson_count,
                 ];
             }
