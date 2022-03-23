6f679e74e997d0340a0d2c64a45aa4669214c5c6
koogua <xiaochong0302@gmail.com>
2022-03-23 09:44:35 +0800

    修正验证码关闭后元素未定义问题

--- public/static/home/js/captcha.login.js
+++ public/static/home/js/captcha.login.js
@@ -2,17 +2,19 @@ layui.use(['jquery'], function () {
 
     var $ = layui.jquery;
 
-    var captcha = new TencentCaptcha(
-        $('#captcha-btn')[0],
-        $('#captcha-btn').data('app-id'),
-        function (res) {
-            if (res.ret === 0) {
-                $('#ticket').val(res.ticket);
-                $('#rand').val(res.randstr);
-                $('#captcha-block').hide();
-                $('#submit-btn').removeClass('layui-btn-disabled').removeAttr('disabled');
+    if ($('#captcha-btn').length > 0) {
+        var captcha = new TencentCaptcha(
+            $('#captcha-btn')[0],
+            $('#captcha-btn').data('app-id'),
+            function (res) {
+                if (res.ret === 0) {
+                    $('#ticket').val(res.ticket);
+                    $('#rand').val(res.randstr);
+                    $('#captcha-block').hide();
+                    $('#submit-btn').removeClass('layui-btn-disabled').removeAttr('disabled');
+                }
             }
-        }
-    );
+        );
+    }
 
 });
