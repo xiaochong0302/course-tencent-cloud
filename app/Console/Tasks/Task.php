30ece511dfda24b4030f4ed3708696552b3dae20
xiaochong0302 <xiaochong0302@gmail.com>
2023-07-03 16:23:11 +0800

    1.优化日志清理 2.增加cli下带色输出

--- app/Console/Tasks/Task.php
+++ app/Console/Tasks/Task.php
@@ -11,5 +11,17 @@ use App\Traits\Service as ServiceTrait;
 
 class Task extends \Phalcon\Cli\Task
 {
+
     use ServiceTrait;
+
+    protected function successPrint($text)
+    {
+        echo "\033[32m {$text} \033[0m" . PHP_EOL;
+    }
+
+    protected function errorPrint($text)
+    {
+        echo "\033[31m {$text} \033[0m" . PHP_EOL;
+    }
+
 }
