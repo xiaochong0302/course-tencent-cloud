2195904e651efaed110d66686999a2c8890d2fd7
xiaochong0302 <xiaochong0302@gmail.com>
2023-12-07 20:57:25 +0800

    删除多余文件

--- app/Repos/TeacherConsult.php
+++ app/Repos/TeacherConsult.php
@@ -9,7 +9,7 @@ namespace App\Repos;
 
 use App\Library\Paginator\Adapter\QueryBuilder as PagerQueryBuilder;
 use App\Models\Consult as ConsultModel;
-use App\Models\CourseUser as CourseUserModel;
+use App\Models\Course as CourseModel;
 
 class TeacherConsult extends Repository
 {
@@ -17,35 +17,34 @@ class TeacherConsult extends Repository
     public function paginate($where, $sort = 'latest', $page = 1, $limit = 15)
     {
         $builder = $this->modelsManager->createBuilder()
-            ->columns('c.*')
-            ->addFrom(ConsultModel::class, 'c')
-            ->join(CourseUserModel::class, 'c.course_id = cu.course_id', 'cu')
+            ->columns('consult.*')
+            ->addFrom(ConsultModel::class, 'consult')
+            ->join(CourseModel::class, 'consult.course_id = course.id', 'course')
             ->where('1 = 1');
 
-        if (!empty($where['user_id'])) {
-            $builder->andWhere('cu.user_id = :user_id:', ['user_id' => $where['user_id']]);
-            $builder->andWhere('cu.role_type = :role_type:', ['role_type' => CourseUserModel::ROLE_TEACHER]);
-            $builder->andWhere('cu.deleted = 0');
+        if (!empty($where['teacher_id'])) {
+            $builder->andWhere('course.teacher_id = :teacher_id:', ['teacher_id' => $where['teacher_id']]);
+            $builder->andWhere('course.published = 1');
+            $builder->andWhere('course.deleted = 0');
         }
 
         if (isset($where['replied'])) {
             if ($where['replied'] == 1) {
-                $builder->andWhere('c.reply_time > 0');
+                $builder->andWhere('consult.reply_time > 0');
             } else {
-                $builder->andWhere('c.reply_time = 0');
+                $builder->andWhere('consult.reply_time = 0');
             }
         }
 
-        $builder->andWhere('c.published = :published:', ['published' => ConsultModel::PUBLISH_APPROVED]);
-
-        $builder->andWhere('c.deleted = 0');
+        $builder->andWhere('consult.published = :published:', ['published' => ConsultModel::PUBLISH_APPROVED]);
+        $builder->andWhere('consult.deleted = 0');
 
         switch ($sort) {
             case 'oldest':
-                $orderBy = 'c.id ASC';
+                $orderBy = 'consult.id ASC';
                 break;
             default:
-                $orderBy = 'c.id DESC';
+                $orderBy = 'consult.id DESC';
                 break;
         }
 
