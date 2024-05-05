bd14979e9b6d90acffe9e72d96be313050287da9
xiaochong0302 <xiaochong0302@gmail.com>
2024-05-05 21:52:13 +0800

    调整编辑器htmlTags属性

--- public/static/home/js/content.editor.js
+++ public/static/home/js/content.editor.js
@@ -17,7 +17,18 @@ layui.use(['jquery'], function () {
             'image', 'link', 'unlink', '|',
             'source', 'about'
         ],
-        pasteType: 1,
+        htmlTags: {
+            span: ['.color', '.background-color'],
+            a: ['id', 'class', 'href', 'target', 'name'],
+            img: ['id', 'class', 'src', 'width', 'height', 'alt', 'title'],
+            table: ['id', 'class'],
+            div: ['id', 'class'],
+            pre: ['id', 'class'],
+            hr: ['id', 'class'],
+            'td,th': ['id', 'class'],
+            'p,ol,ul,li,blockquote,h1,h2,h3,h4,h5,h6': ['id', 'class'],
+            'br,tbody,tr,strong,b,sub,sup,em,i,u,strike,s,del': ['id', 'class'],
+        },
         extraFileUploadParams: {
             csrf_token: $('meta[name="csrf-token"]').attr('content')
         }
