e03dcad2010a8626db230ea538a20ec0b75ca7d7
xiaochong0302 <xiaochong0302@gmail.com>
2024-05-16 21:29:02 +0800

    修正课程列表顶部过滤条件部分无法收缩问题

--- public/static/home/js/course.list.js
+++ public/static/home/js/course.list.js
@@ -7,17 +7,4 @@ layui.use(['jquery', 'helper'], function () {
 
     helper.ajaxLoadHtml($courseList.data('url'), $courseList.attr('id'));
 
-    var $filter = $('.course-filter');
-
-    $('.filter-toggle').on('click', function () {
-        var $icon = $(this).find('.layui-icon');
-        if ($icon.hasClass('layui-icon-up')) {
-            $icon.removeClass('layui-icon-up').addClass('layui-icon-down');
-            $filter.hide();
-        } else {
-            $icon.removeClass('layui-icon-down').addClass('layui-icon-up');
-            $filter.show();
-        }
-    });
-
 });
